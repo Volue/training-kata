d0beff0dce4d01c065ca3d48ad2a2e98995eea92
Jaroslaw Krefta <jaroslaw.krefta@volue.com>
2023-03-22 16:29:00 +0100

    Sesja z 2023-03-22

--- Training.Kata/Training.Kata.BarcodeScanner.Api/Controllers/BarcodeController.cs
+++ Training.Kata/Training.Kata.BarcodeScanner.Api/Controllers/BarcodeController.cs
@@ -17,12 +17,47 @@ public class BarcodeController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
-        return NotFound();
+        return Ok(_repository.Barcodes);
     }
 
     [HttpGet("scan")]
     public IActionResult Scan(string barcode)
     {
+        if (_repository.Barcodes.ContainsKey(barcode))
+        {
+            return Ok(_repository.Barcodes[barcode]);
+        }
+
         return NotFound();
+    }
+    
+    [HttpGet("search")]
+    public IActionResult Search(
+        [FromQuery]string? name, 
+        [FromQuery]string? description)
+    {
+        if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(description))
+        {
+            return NotFound();
+        }
+
+        if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(description))
+        {
+            return Ok(_repository.Barcodes.Where(x =>
+            {
+                return x.Value.Name.Contains(name) 
+                       && x.Value.Description.Contains(description);
+            }));
+        }
+        
+        if (!string.IsNullOrEmpty(description))
+        {
+            return Ok(_repository.Barcodes.Where(x =>
+            {
+                return x.Value.Description.Contains(description);
+            }));
+        }
+        
+        return Ok(_repository.Barcodes.Where(x => x.Value.Name.Contains(name)));
     }
 }
