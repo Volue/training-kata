a2bc88250f9a5c0e582bc02e1c473b4c928d8dc3
Jarosław Krefta <jaroslaw.krefta@volue.com>
2024-01-17 15:57:04 +0100

    Sesja z dnia 2024-01-17

---
+++ Training.Kata/Volue.BattleShips.Api/CellViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Volue.BattleShips.Api;
+
+// Obiekt reprezentujący dane wychodzący lub przychodzący z zewnątrz aplikacji
+public class CellViewModel
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsShip { get; private set; }
+    public bool IsHit { get; set; }
+    
+//  dostęo  konstruktor    Klasa zmiennej| Nazwa zmiennej
+    public   CellViewModel(      Cell       domainModel)
+    {
+        X = domainModel.X;
+        Y = domainModel.Y;
+        IsShip = domainModel.IsShip;
+        IsHit = domainModel.IsHit;
+    }
+}
+
+
+
+// Obiekt domenowy zawierający logike
+public class Cell
+{
+    public int X;
+    public int Y;
+    public bool IsShip;
+    public bool IsHit;
+
+    public void SetHit()
+    {
+        IsHit = true;
+    }
+}
+
---
+++ Training.Kata/Volue.BattleShips.Api/Program.cs
@@ -0,0 +1,67 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Volue.BattleShips.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var basicCell = new Cell
+{
+    X = 1,
+    Y = 2
+};
+
+var cellViewModel = new CellViewModel(basicCell);
+
+var serializedBasicCell = JsonSerializer.Serialize(basicCell);
+var serializedCellViewModel = JsonSerializer.Serialize(cellViewModel);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapPost("/ship", (CellViewModel cell) =>
+    {
+        return "";
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
